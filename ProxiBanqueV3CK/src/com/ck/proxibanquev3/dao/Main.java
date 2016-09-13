7b3c25718baa532c8dc592adcab2970469b64a3b
Stagiaire <Stagiaire@poste_master.home>
2016-09-13 14:23:29 +0200

    main dans dao

--- ProxiBanqueV3CK/src/com/ck/proxibanquev3/dao/Main.java
+++ ProxiBanqueV3CK/src/com/ck/proxibanquev3/dao/Main.java
@@ -1,33 +1,51 @@
 package com.ck.proxibanquev3.dao;
 
 import com.ck.proxibanquev3.domaine.Client;
-import com.ck.proxibanquev3.domaine.Conseiller;
 import com.ck.proxibanquev3.domaine.Courant;
+import com.ck.proxibanquev3.domaine.Epargne;
 
 public class Main {
 	
 	public static void main(String[] args) {
-
-		Conseiller conseiller1 = new Conseiller("castro", "clement", "cca", "gtm");
-		Client client1 = new Client("pierre", "jacque");
-		Client client2 = new Client("Amanda", "Stark");
-		ConseillerDAO conseillerDAO = new ConseillerDAO();
-		ClientDAO clientDAO = new ClientDAO();
-		CompteDAO compteDAO = new CompteDAO();
-		Client client3;
-		Courant courant1 = new Courant(540, "12/08/2015", client1);
 		
-		clientDAO.createClient(client1);
-		conseillerDAO.createConseiller(conseiller1);
-		clientDAO.createClient(client2);
-		System.out.println(clientDAO.getAllClient());
-		clientDAO.deleteClient(client1);
-		System.out.println(clientDAO.getAllClient());
-		client3 = clientDAO.findClientById(7);
-		client3.setNom("Jean");
-		clientDAO.updateClient(client3);
-		System.out.println(clientDAO.getAllClient());
-		compteDAO.createCourant(courant1);
+//		Courant courant1=new Courant(1500.0);
+//		Epargne epargne1=new Epargne(20000.0);
+//
+//		Client cl1=new Client("Hallyday", "Johny","75 rue des stars", "johny@gmail.com", courant1, epargne1);
+//		
+//		Courant courant2=new Courant(300.0);
+//		Epargne epargne2=new Epargne(500.0);
+//		Client cl2=new Client("Marley", "Bob","37 avec des joined", "bobby@gmail.com", courant2, epargne2);
+//		
+//		Courant courant3=new Courant(1800.0);
+//		Epargne epargne3=new Epargne(5000.0);
+//		Client cl3=new Client("Morose", "Marilyn","84 impasse des actrices", "marilyn@gmail.com", courant3, epargne3);
+//		
+//		
+//		Courant courant4=new Courant(1500.0);
+//		Epargne epargne4=new Epargne(20000.0);
+//
+//		Client cl4=new Client("Delahousse", "Jean","75 rue des animateurs", "jean@gmail.com", courant4, epargne4);
+//		
+//		Courant courant5=new Courant(300.0);
+//		Epargne epargne5=new Epargne(500.0);
+//		Client cl5=new Client("Tatcher", "margarette","56 rue main de fer", "margarette@gmail.com", courant5, epargne5);
+//		
+//		Courant courant6=new Courant(1800.0);
+//		Epargne epargne6=new Epargne(5000.0);
+//		Client cl6=new Client("Waller", "Amanda","78 rue de la paix", "greenpeace@gmail.com", courant6, epargne6);
+//		
+//
+//		ClientDAO clientDAO = new ClientDAO();
+//		
+//
+//		clientDAO.createClient(cl1);
+//		clientDAO.createClient(cl2);
+//		clientDAO.createClient(cl3);
+//		clientDAO.createClient(cl4);
+//		clientDAO.createClient(cl5);
+//		clientDAO.createClient(cl6);
+		
 		
 	}
 
